c437f2fdffe5cb9d417f1860e2903c2c443787c4
Kenneth Jones <kenneth.jones@vocus.com.au>
2017-07-28 17:55:24 +1000

    Some missing files for testing...

---
+++ TestMotionScript/run.py
@@ -0,0 +1,8 @@
+# Test Run
+import os, sys, inspect
+currentDir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentDir = os.path.dirname(currentDir)
+sys.path.insert(0,parentDir)
+import MotionScript
+debug = False
+MS = MotionScript.MotionScript(verbose=debug, debug=debug, config="ParticleTest.json")
